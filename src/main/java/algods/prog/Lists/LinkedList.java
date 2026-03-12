839a87eaa2b89e1c23e5a23eb9ca876cf6ad68e5
JensSchmidt22 <jens.schmidt2007@gmail.com>
2026-03-12 14:02:37 +0100

    b

---
+++ src/main/java/algods/prog/Lists/LinkedList.java
@@ -0,0 +1,36 @@
+package algods.lecture.vl_2026_03_10.Lists;
+
+public class LinkedList {
+    Element anchor;
+    int size;
+
+    public LinkedList() {
+        this.anchor = new Element();
+    }
+
+    /// Hängt einen neuen Wert am Ende der Liste an.
+    public void add(int data) {
+        this.anchor.add(data);
+        this.size++;
+    }
+
+    /// Fügt einen neuen Wert an der gegeben Position in die Liste ein.
+    public void insert(int pos, int data) {
+        // TODO
+        
+    }
+
+    /// Entfernt das letzte Element aus der Liste.
+    public void remove_last() {
+        // TODO
+    }
+
+    /// Entfernt das Element an der gegebene Position aus der Liste.
+    public void remove(int pos) {
+        // TODO
+    }
+
+    public int size() {
+        return this.size;
+    }
+}
